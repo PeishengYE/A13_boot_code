2c9e61e14e361049c3f4453d5e725acec4121cc5
kevin <kevin@allwinnertech.com>
2012-09-14 19:34:33 +0800

    A13-Fire-Jelly-Bean-v1.0 release

---
+++ boot1/driver/drv_de/de_bsp/iep/disp_iep.c
@@ -0,0 +1,33 @@
+#include "disp_iep.h"
+
+__s32 DE_IEP_Set_Reg_Base(__u32 sel, __u32 base)
+{
+    return 0;
+}
+
+__s32 Disp_iep_exit(__u32 sel)
+{
+    return 0;
+}
+
+__s32 Disp_iep_init(__u32 sel)
+{
+    return 0;
+}
+
+__s32 IEP_Operation_In_Vblanking(__u32 sel, __u32 tcon_index)
+{
+    return 0;
+}
+
+__s32 Disp_de_flicker_enable(__u32 sel, __u32 en)
+{
+    return 0;
+}
+
+__s32 Disp_drc_enable(__u32 sel, __u32 en)
+{
+    return 0;
+}
+
+
